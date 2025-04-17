3fd50c9fc6a8e0b54ba365c829386fe3bc9108d7
Arnav Ghosh <arnavghosh.bca.23@sittechno.edu.in>
2025-04-18 02:46:29 +0530

    Fuck NextJS

--- app/(root)/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div>
-      <p className="text-3xl font-bold underline">Hello, World!</p>
-    </div>
-  );
-}
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import LandingPage from "@/components/landing-page"
-
-export default function Home() {
-  return <LandingPage className="border-4" />
-}
